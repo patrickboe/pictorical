d5e2d88cdeb1ee0c2a19bcae3683ba93f5c1090f
patrick <patrickboe@gmail.com>
2010-09-24 14:15:59 -0400

    made a generic photo interface

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
+//extend google's LatLng to include a distance finding method
+google.maps.LatLng.prototype.distanceTo=
+// This distance formula is adapted slightly for google's object model from Chris Veness' code under the following copyright:
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
+/*  Latitude/longitude spherical geodesy formulae & scripts (c) Chris Veness 2002-2010            */
+/*   - www.movable-type.co.uk/scripts/latlong.html                                                */
+/*                                                                                                */
+/*  Sample usage:                                                                                 */
+/*    var p1 = new LatLon(51.5136, -0.0983);                                                      */
+/*    var p2 = new LatLon(51.4778, -0.0015);                                                      */
+/*    var dist = p1.distanceTo(p2);          // in km                                             */
+/*    var brng = p1.bearingTo(p2);           // in degrees clockwise from north                   */
+/*    ... etc                                                                                     */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
+/**
+ * Returns the distance from this point to the supplied point, in meters 
+ * (using Haversine formula)
+ *
+ * from: Haversine formula - R. W. Sinnott, "Virtues of the Haversine",
+ *       Sky and Telescope, vol 68, no 2, 1984
+ *
+ * @param   {LatLon} point: Latitude/longitude of destination point
+ * @param   {Number} [precision=4]: no of significant digits to use for returned value
+ * @returns {Number} Distance in meters between this point and destination point
+ */
+function(point, precision) {
+  // default 4 sig fig reflects typical 0.3% accuracy of spherical model
+  if (typeof precision == 'undefined') precision = 4;  
+  
+  var R = 6371;
+  var toRadians=function(deg){return Math.PI*deg/180};
+  var lat1 = toRadians(this.lat()), lon1 = toRadians(this.lng());
+  var lat2 = toRadians(point.lat()), lon2 = toRadians(point.lng());
+  var dLat = lat2 - lat1;
+  var dLon = lon2 - lon1;
+
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+          Math.cos(lat1) * Math.cos(lat2) * 
+          Math.sin(dLon/2) * Math.sin(dLon/2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d = R * c * 1000;
+  return d.toFixed(precision);
+}
+
 pictorical={
 		loadMap: function(selectionCallback) {
 			var map;
@@ -7,7 +51,7 @@ pictorical={
 				mapClickListener=google.maps.event.addListener(map,"click",lambda);
 			}
 			var findDistance=function(locA,locB){
-				return Math.round(distVincenty(locA.lat(),locA.lng(),locB.lat(),locB.lng()));
+				return Math.round(locA.distanceTo(locB));
 			};
 			var drawStartingMap=function(){
 				var HardenaRestaurant=new google.maps.LatLng(39.928431,-75.171257);
@@ -67,72 +111,26 @@ pictorical={
 				};
 				updateMapClickBehavior(cancelSelection);
 			};
-
-			var distVincenty=
-			// This distance calculation formula is from http://www.movable-type.co.uk/scripts/latlong-vincenty.html
-			/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
-			/* Vincenty Inverse Solution of Geodesics on the Ellipsoid (c) Chris Veness 2002-2010             */
-			/*                                                                                                */
-			/* from: Vincenty inverse formula - T Vincenty, "Direct and Inverse Solutions of Geodesics on the */
-			/*       Ellipsoid with application of nested equations", Survey Review, vol XXII no 176, 1975    */
-			/*       http://www.ngs.noaa.gov/PUBS_LIB/inverse.pdf                                             */
-			/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
-
-			/**
-			 * Calculates geodetic distance between two points specified by latitude/longitude using 
-			 * Vincenty inverse formula for ellipsoids
-			 *
-			 * @param   {Number} lat1, lon1: first point in decimal degrees
-			 * @param   {Number} lat2, lon2: second point in decimal degrees
-			 * @returns (Number} distance in metres between points
-			 */
-			function(lat1, lon1, lat2, lon2) {
-			  var a = 6378137, b = 6356752.314245,  f = 1/298.257223563;  // WGS-84 ellipsoid params
-			  // replaced original calls to a missing toRad() function with a custom toRadians() function - Patrick Boe
-			  var toRadians=function(deg){return Math.PI*deg/180}
-			  var L = toRadians(lon2-lon1);
-			  var U1 = Math.atan((1-f) * Math.tan(toRadians(lat1)));
-			  var U2 = Math.atan((1-f) * Math.tan(toRadians(lat2)));
-			  var sinU1 = Math.sin(U1), cosU1 = Math.cos(U1);
-			  var sinU2 = Math.sin(U2), cosU2 = Math.cos(U2);
-			  
-			  var lambda = L, lambdaP, iterLimit = 100;
-			  do {
-			    var sinLambda = Math.sin(lambda), cosLambda = Math.cos(lambda);
-			    var sinSigma = Math.sqrt((cosU2*sinLambda) * (cosU2*sinLambda) + 
-			      (cosU1*sinU2-sinU1*cosU2*cosLambda) * (cosU1*sinU2-sinU1*cosU2*cosLambda));
-			    if (sinSigma==0) return 0;  // co-incident points
-			    var cosSigma = sinU1*sinU2 + cosU1*cosU2*cosLambda;
-			    var sigma = Math.atan2(sinSigma, cosSigma);
-			    var sinAlpha = cosU1 * cosU2 * sinLambda / sinSigma;
-			    var cosSqAlpha = 1 - sinAlpha*sinAlpha;
-			    var cos2SigmaM = cosSigma - 2*sinU1*sinU2/cosSqAlpha;
-			    if (isNaN(cos2SigmaM)) cos2SigmaM = 0;  // equatorial line: cosSqAlpha=0 (§6)
-			    var C = f/16*cosSqAlpha*(4+f*(4-3*cosSqAlpha));
-			    lambdaP = lambda;
-			    lambda = L + (1-C) * f * sinAlpha *
-			      (sigma + C*sinSigma*(cos2SigmaM+C*cosSigma*(-1+2*cos2SigmaM*cos2SigmaM)));
-			  } while (Math.abs(lambda-lambdaP) > 1e-12 && --iterLimit>0);
-
-			  if (iterLimit==0) return NaN  // formula failed to converge
-
-			  var uSq = cosSqAlpha * (a*a - b*b) / (b*b);
-			  var A = 1 + uSq/16384*(4096+uSq*(-768+uSq*(320-175*uSq)));
-			  var B = uSq/1024 * (256+uSq*(-128+uSq*(74-47*uSq)));
-			  var deltaSigma = B*sinSigma*(cos2SigmaM+B/4*(cosSigma*(-1+2*cos2SigmaM*cos2SigmaM)-
-			    B/6*cos2SigmaM*(-3+4*sinSigma*sinSigma)*(-3+4*cos2SigmaM*cos2SigmaM)));
-			  var s = b*A*(sigma-deltaSigma);
-			  
-			  s = s.toFixed(3); // round to 1mm precision
-			  return s;
-			};
 			
 			map=drawStartingMap();
 			acceptSelections();
 		},
 		
 		displayAreaPhotos: function(selectedCircle){
-			var yqlQuery="select * from flickr.photos.search(0,200)" +
+			var acceptPhotos=function(photos){
+				if(selectedCircle.getMap()!=null){
+					photos.sort(function(a,b){
+						if(a.getDate() > b.getDate()) return 1;
+						return -1;
+					});
+					pictorical.showSlides(photos);
+				}
+			}
+			pictorical.requestFlickrPhotos(selectedCircle,acceptPhotos);
+		},
+		
+		requestFlickrPhotos: function(selectedCircle, photosFoundCallback){
+			var yqlQuery="select * from flickr.photos.search(0,5)" +
 			" where lat="+ String(selectedCircle.getCenter().lat()) +
 			" and lon="+ String(selectedCircle.getCenter().lng()) +
 			" and radius="+ String(selectedCircle.getRadius()/1000.0) +
@@ -145,76 +143,82 @@ pictorical={
 						format: "json"
 				},
 				function(data){
-					if(selectedCircle.getMap()!=null){
-						pictorical.acceptFlickrResults(data);
+					var photos=[];
+					if(!!data.query.results){
+						if(data.query.count==1){
+							//make the result an array
+							photos.push(data.query.results.photo);
+						} else {
+							//it's already an array
+							photos=data.query.results.photo;
+						}
 					}
+					transformArray(photos,function(photo){return new pictorical.flickrPhoto(photo)})
+					photosFoundCallback(photos)
 				}
 			);
 		},
 		
-		acceptFlickrResults:function(data){
-			var photos=[];
-			if(!!data.query.results){
-				if(data.query.count==1){
-					//make the result an array
-					photos.push(data.query.results.photo);
-				} else {
-					//it's already an array
-					photos=data.query.results.photo;
-				}
-				photos.sort(function(a,b){
-					if(a.datetaken > b.datetaken) return 1;
-					return -1;
-				});
-				this.showSlides(photos);
-			}
-		},
-		
 		showSlides: function(photos){
+			var adjustImageMap=function(){
+				   //set up img map areas for current photo
+				   var $this=$(this);
+				   var img=$this.find("img")[0];
+				   var areaW=Math.round(img.clientWidth/2)-5
+				   var prevRightEdge=String(areaW);
+				   var nextLeftEdge=String(areaW+10);
+				   var w=String(img.clientWidth);
+				   var h=String(img.clientHeight);
+				   $this
+				   	.find("area.prev")
+				   		.attr("coords","0,0,"+prevRightEdge+","+h)
+				   	.end()
+				   	.find("area.next")
+			   			.attr("coords", nextLeftEdge+",0,"+w+","+h)
+			   };
 			var $photoList=$("#slideshow ul");
-			var toFlickrUrl=function(photo){
-				return "http://farm"+photo.farm+".static.flickr.com/"+photo.server+"/"+photo.id+"_"+photo.secret+".jpg"
-			};
 			var loadPhoto=function(photo){
-				$photoList.append('<li><label>'+String(photo.datetaken)+'</label>'+
-				'<img usemap="p'+photo.id+'" src="'+toFlickrUrl(photo)+'"/>'+
-				'<map name="p'+photo.id+'">'+
-				'<area shape="rect" class="prev" coords="0,0,40,40" href="#prev" title="Return to Previous Photo" alt="Previous" />'+
-				'<area shape="rect" class="next" coords="50,0,90,40" href="#next" title="Advance to Next Photo" alt="Next" />'+
+				$photoList.append('<li><label>'+String(photo.getDate())+'</label>'+
+				'<img usemap="#p'+photo.getID()+'" src="'+photo.getUrl()+'"/>'+
+				'<map name="p'+photo.getID()+'">'+
+				'<area shape="rect" class="prev" coords="0,0,40,40" href="#prev" title="Return to Previous Photo" alt="Previous"/>'+
+				'<area shape="rect" class="next" coords="50,0,90,40" href="#next" title="Advance to Next Photo" alt="Next"/>'+
 				'</map></li>');
 			};
+			//add photos to the DOM
 			for(var i in photos){
 				loadPhoto(photos[i]);
 			}
-			$("#map_canvas").hide();
-			$("#slideshow").show().find("ul.slideshow")
-				.cycle({
-					   timeout: 0,
-					   prev:   '.prev', 
-					   next:   '.next',
-					   after:	function(){
-						   //set up img map areas
-						   var $this=$(this);
-						   var img=$this.find("img")[0];
-						   var areaW=img.clientWidth/2-5
-						   var prevRightEdge=String(areaW);
-						   var nextLeftEdge=String(areaW+10);
-						   var w=String(img.clientWidth)-5;
-						   var h=String(img.clientHeight)-5;
-						   $this
-						   	.find("area.prev")
-						   		.attr("coords","5,5,"+prevRightEdge+","+h)
-						   	.end()
-						   	.find("area.next")
-					   			.attr("coords", nextLeftEdge+",5,"+w+","+h)
-					   }
-				   });
+			//set the images to cycle once the first one loads
+			$("#slideshow img:first").load(function(){
+				$("#map_canvas").hide();
+				$("#slideshow").show()
+					.find("ul.slideshow")
+						.cycle({
+						   timeout: 0,
+						   prev:   '.prev', 
+						   next:   '.next',
+						   after:	adjustImageMap
+						});
+			});
 		},
 		
 		showMap: function(){
 			$("#slideshow").hide().find("ul.slideshow").empty();
 			$("#map_canvas").show();
+		},
+		
+		flickrPhoto: function(rawPhoto){
+			this.getID=function(){return "flickr"+String(rawPhoto.id)}
+			this.getUrl=function(){return "http://farm"+rawPhoto.farm+".static.flickr.com/"+rawPhoto.server+"/"+rawPhoto.id+"_"+rawPhoto.secret+".jpg";};
+			this.getDate=function(){return rawPhoto.datetaken};
 		}
+}
+
+transformArray=function(arr,lambda){
+	for (var i in arr){
+		arr[i]=lambda(arr[i]);
+	}
 }
 
 $(function(){
