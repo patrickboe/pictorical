96ac0362408e4fb11434c474f3dface246788f53
patrick <patrickboe@gmail.com>
2010-10-20 08:00:01 -0400

    cleaned up

--- src/resources/hyde/emacs/hyde.el
+++
@@ -1,7 +0,0 @@
-;;hyde templates functions
-(defun hyde-new-draft()
-  "Init a new blog post for hyde engine"
-  (interactive)
-  (insert "{% extends \"_post.html\" %}\n{%hyde\n   title:\n   categories:\n   created:\n%}\n{% block article %}\n{% article %}\n\n{% endarticle %}\n{% endblock %}")
-  (goto-line 8)
-)
--- src/resources/hyde/hydeengine/content_processors.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-
-class PassthroughProcessor:
-
-   @staticmethod
-   def process(resource):       
-	   resource.prerendered = True
-
