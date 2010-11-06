e262d335ee4dcef21253da48ee665f62f0e6487d
patrick <patrickboe@gmail.com>
2010-11-06 19:04:26 -0400

    copy, style edits

--- pavement.py
+++ pavement.py
@@ -11,7 +11,7 @@ sys.path.append(buildDir.abspath())
 from buildconfig import loadConf
 
 options(
-        version="0.2.5",
+        version="0.2.6",
         version_date="11/06/2010",
         targetEnv="local"
         )
